2425903834ceadc7a1e24a32281c750cbb18f435
kevin <kevin.vonahausen@gmail.com>
2021-12-16 23:47:50 -0300

    Showing activities on itinerary page (no design done yet)

--- frontend/src/components/city/ActivitiesSection.js
+++ frontend/src/components/city/ActivitiesSection.js
@@ -5,30 +5,35 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Gear from "../../assets/gear.png";
-import {connect} from "react-redux"
+import { connect } from "react-redux";
 import itinerariesActions from "../../redux/actions/itinerariesActions";
-import {useEffect} from 'react'
+import { useEffect } from "react";
 
 function ActivitiesSection(props) {
   const [open, setOpen] = useState(false);
 
-  console.log('esta es la lista de activities: ')
-  console.log(props.activities)
+  console.log("COMPONENT: esta es la lista de activities: ");
+  console.log(props.activities);
 
-  console.log('estos son los id de itinerarios que existen')
-  props.activities.map(activity=> console.log(activity.itinerary[0]._id))
-//   sacar de aqui los valores unicos, para luego compararlo con el props.id. Si son distintos, entonces que haga un pedido a la database
+  //   console.log('estos son los id de itinerarios que ya estan cargados en el store principal')
+  //   props.activities.map(activity=> console.log(activity.itinerary[0]._id))
+  //   console.log('y este es la nueva peticion de id '+ props.id)
+  //   sacar de aqui los valores unicos, para luego compararlo con el props.id. Si son distintos, entonces que haga un pedido a la database
 
-//   useEffect(()=>{
-//       props.getActivities(props.id)
-//   },[])
+  //   useEffect(()=>{
+  //       props.getActivities(props.id)
+  //   },[])
 
-  const handleClick=()=>{
-      setOpen(!open)
-      props.getActivities(props.id, props.activities)
+  const handleClick = () => {
+    setOpen(!open);
+    props.getActivities(props.id, props.activities);
     //   aqui se le pasa la lista actual, para que en el action se verifique si es que el props.id (el id del itinerario) esta en
     // props.activities. props.activities.map(e=>e.itinerary===id)
-  }
+  };
+
+  let activityList = props.activities.filter(
+    (activity) => activity.itinerary[0]._id === props.id
+  );
 
   return (
     <Container
@@ -47,12 +52,20 @@ function ActivitiesSection(props) {
       <Collapse in={open}>
         <Container id="example-collapse-text" className="pt-1">
           <Row className="d-flex flex-column flex-lg-row">
-            <Col className="text-center bg-info p-3 m-1">
+            <Col sm={8} className="text-center bg-info p-3">
               <p className="display-6">Activities</p>
-              <img src={Gear} alt="gear" id="gear" />
-              <p className="text-light fw-bold">Under Construction</p>
+              {activityList.map((activity) => 
+                  <>
+                {/* <div className="activity-image" style={{backgroundImage: `url(${activity.image})`}}></div> */}
+                <Container className="activity-image" fluid style={{backgroundImage: `url(${activity.image})`}}></Container>
+                {/* <img className="activity-image" src={activity.image} alt={activity.name} /> */}
+                <p>{activity.name}</p>
+                <p>{activity.description}</p>
+                <p>{activity.duration}</p>
+                  </>
+              )}
             </Col>
-            <Col className="text-center bg-warning p-3 m-1">
+            <Col sm={4} className="text-center bg-warning p-3">
               <p className="display-6">Comments</p>
               <img src={Gear} alt="gear" id="gear" />
               <p className="text-light fw-bold">Under Construction</p>
@@ -64,13 +77,13 @@ function ActivitiesSection(props) {
   );
 }
 const mapStateToProps = (state) => {
-    return {
-      activities:state.itinerariesReducer.activities
-    };
+  return {
+    activities: state.itinerariesReducer.activities,
   };
-  
-  const mapDispatchToProps = {
-    getActivities : itinerariesActions.getActivities
-  };
+};
 
-export default connect(mapStateToProps,mapDispatchToProps)(ActivitiesSection);
+const mapDispatchToProps = {
+  getActivities: itinerariesActions.getActivities,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ActivitiesSection);
--- frontend/src/redux/actions/itinerariesActions.js
+++ frontend/src/redux/actions/itinerariesActions.js
@@ -14,12 +14,31 @@ const itinerariesActions = {
       dispatch({ type: "SET_LOAD", payload: true });
     };
   },
-  getActivities : (id)=>{
+  getActivities : (id, activities)=>{
       return async(dispatch)=>{
-          console.log('este id me llega desde el component: ' + id)
-          let res = await axios.post("http://localhost:4000/api/activities",{id:id})
-          console.log(res.data.response)
-          dispatch({type: "GET_ACTIVITIES", payload: {itineraryId: id, activityList: res.data.response}})
+          let idList = [...new Set(activities.map(activity=> activity.itinerary[0]._id))]
+          console.log('ACTION: esta es la lista de ids existentes')
+          console.log(idList)
+          let res = []
+          if(idList.includes(id)){
+            console.log('ACTION: se encontro un id existente. NO MOLESTAMOS A LA BASE DE DATOS')
+            
+            res = activities
+            console.log('ACTION: la misma lista que despacho el componente, se le devuelve')
+            console.log(res)
+              
+          }else{
+              
+            console.log('ACTION: el id no existe, molestamos a la base de datos')
+              let resDb = await axios.post("http://localhost:4000/api/activities",{id:id})
+              res = resDb.data.response
+              console.log('ACTION: se obtuvo desde la DB')
+              console.log(res)
+          }
+        //   console.log('ACTION: se cargan las actividades con id de itinerario ' + id)
+        
+        //   console.log(res.data.response)
+          dispatch({type: "GET_ACTIVITIES", payload: {itineraryId: id, activityList: res}})
           
       }
   }
