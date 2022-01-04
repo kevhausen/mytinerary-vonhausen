c664f7e03763146e4a5a5e667f56b424feacdbac
Kevhausen <kevin.vonahausen@gmail.com>
2022-01-04 20:41:11 -0300

    heroku if

--- server.js
+++ server.js
@@ -5,12 +5,26 @@ const router = require("./routers/routes.js");
 require("./config/database.js");
 const passport = require('passport')
 const app = express();
+const path = require('path')
+
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize())
 
 app.use("/api", router);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    });
+  }
+
+
+
 
 const PORT = process.env.PORT || 4000
 const HOST = process.env.HOST || '0.0.0.0'
